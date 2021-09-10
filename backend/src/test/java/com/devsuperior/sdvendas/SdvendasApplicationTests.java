1844b71a5f8b63a33aca9fad31a6512ee774a3a1
Kayo Victor <kayoamorim@gmail.com>
2021-09-10 11:03:12 -0300

    Projeto Criado

---
+++ backend/src/test/java/com/devsuperior/sdvendas/SdvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.sdvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
