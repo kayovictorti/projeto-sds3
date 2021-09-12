3c0b7ab973235260af197c299be34a0a7779c45a
Kayo Victor <kayoamorim@gmail.com>
2021-09-12 15:30:27 -0300

    Layers

---
+++ backend/src/main/java/com/devsuperior/sdvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.sdvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.sdvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long> {
+
+}
